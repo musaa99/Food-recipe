e4ad1ef41cb905fe701e8e0eece1825e877affc5
Musa Opaluwa <innovate.musa.opaluwa@gmail.com>
2023-04-27 23:32:48 +0100

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<div className="flex flex-col md:px-12 px-0 relative bg-background font-raleway items-center min-h-screen">
+			<h1 className="text-6xl font-bold text-active mt-20">
+				Recipe Search
+			</h1>
+			<h2 className="text-[red] text-2xl font-light mt-5">
+				Search recipes from all over the world.
+			</h2>
+		</div>
+	);
+}
